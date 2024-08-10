54e436aa54c88a5a4253530fea1a7b75329ddf87
Priyanshu Arya <priyanshuarya22.gmail.com>
2024-08-10 11:31:44 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in Python these days.
+      I've made a couple of programs in that language. I also like JavaScript and C. Anyway regardless of the language, I just like programming.
+      Here is my <a href="https://github.com/priyanshuarya22" target="_blank">Github</a>!`,
+    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently. I made this portfolio with Kaboom.js after watching a tutorial by <a href="https://www.youtube.com/@JSLegendDev">JSLegendDev</a>. He's great!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1Ae4q-l3WeHDBAl6uJwTf0MpLMv0mD2JN/view?usp=sharing" target="_blank">Check it out?</a>
+    Contact me at priyanshuarya22@gmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+    library: `There are a lot of programming books on my shelves. 
+    I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+    exit: `If you want to exit Priyanshu Arya's portfolio, just close the tab.`,
+};
